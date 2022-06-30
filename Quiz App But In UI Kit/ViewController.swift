67b7f47e195ff12a50baea03170afd6c6fa0de95
DWizard11 <daniyalisawesome111@gmail.com>
2022-06-30 23:04:40 +0800

    Added alert and title and stuff. Error exist with variable not able to be detected

--- Quiz App But In UI Kit/ViewController.swift
+++ Quiz App But In UI Kit/ViewController.swift
@@ -7,17 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        answerTextField.resignFirstResponder()
+        return false
+    }
+    
+    
+   
     @IBOutlet weak var answerTextField: UITextField!
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        answerTextField.delegate = self
     }
 
     @IBAction func enterButtonPressed(_ sender: Any) {
+        if answerTextField.text?.lowercased() == "swift" {
+            var title = "Correct!"
+            var message = "✅"
+        } else {
+            var title = "Wrong"
+            var message = "❌"
+        }
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true)
     }
     
     @IBAction func screenTapped(_ sender: Any) {
