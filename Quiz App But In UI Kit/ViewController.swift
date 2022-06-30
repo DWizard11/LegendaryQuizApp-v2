7900fa791600b9f87031213677c47f61a73239eb
DWizard11 <daniyalisawesome111@gmail.com>
2022-06-30 22:34:02 +0800

    Initial Commit

---
+++ Quiz App But In UI Kit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz App But In UI Kit
+//
+//  Created by DWizard11 on 30/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
